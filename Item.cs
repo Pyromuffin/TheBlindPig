ba2f6221f16dface25e2f40c778e1b8404cd67d1
Kelly MacNeill <kelly@pyromuffin.com>
2023-04-29 14:50:09 -0700

    you can drop one item

--- Item.cs
+++ Item.cs
@@ -10,38 +10,53 @@ public partial class Item : Sprite2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		world = GetParent();
 	}
 
 
+	private Node world;
 	private Node2D holder;
 	private bool overlapped;
+	private bool held = false;
+	
 	
 	private void _on_area_2d_body_entered(Node2D body)
 	{
 		overlapped = true;
 		holder = body;
-		GD.Print("entered");
 	}
 	
 	private void _on_area_2d_body_exited(Node2D body)
 	{
 		overlapped = false;
-		GD.Print("exited");
 	}
 
 
 	public override void _Process(double delta)
 	{
 		
-		if(overlapped){
+		if(held &&  Input.IsActionJustPressed("ui_accept")){
+			// put it down?
+			var parent = holder;
+			var parentPos = holder.Position;
+			parent.RemoveChild(this);
+			world.AddChild(this);
+			Position = parentPos;
+			held = false;
+		}
+		
+		
+		if(overlapped && !held){
 			var selected = Input.IsActionJustPressed("ui_accept");
 			if(selected){
-				GD.Print("item taken");
+				held = true;
 				GetParent().RemoveChild(this);
 				holder.AddChild(this);
 				Position = heldOffset;
 			}
 		}
+		
+	
 		 
 	}
 	

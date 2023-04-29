4f3c285a796b622311ecd13db336244764c39767
Kelly MacNeill <kelly@pyromuffin.com>
2023-04-29 14:26:00 -0700

    animated character

--- Item.cs
+++ Item.cs
@@ -8,18 +8,38 @@ public partial class Item : Sprite2D
 	{
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+
 	
+	private bool overlapped;
 	
 	private void _on_area_2d_body_entered(Node2D body)
 	{
-		GD.Print("overlapped");
+		overlapped = true;
+		GD.Print("entered");
 	}
+	
+	private void _on_area_2d_body_exited(Node2D body)
+	{
+		overlapped = false;
+		GD.Print("exited");
+	}
+
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		
+		if(overlapped){
+			//var selected = Input.IsActionJustPressed("ui_ok");	
+			
+		}
+		 
+	}
+	
 
 	
 }
+
+
 
 

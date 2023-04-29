91892249974212e4b60597799f70aee43e50d8c2
Kelly MacNeill <kelly@pyromuffin.com>
2023-04-29 14:34:56 -0700

    you can pick up one item

--- Item.cs
+++ Item.cs
@@ -3,18 +3,23 @@ using System;
 
 public partial class Item : Sprite2D
 {
+	
+	[Export]
+	public Vector2 heldOffset = new Vector2(0,1);
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 	}
 
 
-	
+	private Node2D holder;
 	private bool overlapped;
 	
 	private void _on_area_2d_body_entered(Node2D body)
 	{
 		overlapped = true;
+		holder = body;
 		GD.Print("entered");
 	}
 	
@@ -25,13 +30,17 @@ public partial class Item : Sprite2D
 	}
 
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		
 		if(overlapped){
-			//var selected = Input.IsActionJustPressed("ui_ok");	
-			
+			var selected = Input.IsActionJustPressed("ui_accept");
+			if(selected){
+				GD.Print("item taken");
+				GetParent().RemoveChild(this);
+				holder.AddChild(this);
+				Position = heldOffset;
+			}
 		}
 		 
 	}

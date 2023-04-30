bf43bac692c54879297e011ec5f79f44590f4083
Loveschach <codymalach@hotmail.com>
2023-04-30 16:43:50 -0700

    Voice Generation

--- Patron.cs
+++ Patron.cs
@@ -158,6 +158,7 @@ public partial class Patron : Sprite2D
 	public void Init(PatronDetails d) {
 		details = d;
 		Texture = GD.Load<Texture2D>("res://assets/characters/" + details.patronType.ToString().ToLower() + ".png");
+		patronVoice.Init( details.patronType );
 	}
 
 	void ResetDialog()
@@ -340,6 +341,10 @@ public partial class Patron : Sprite2D
 	public void CreateDialog(string text){
 		currentDialog = text;
 		EnterState( State.TALKING );
+	}
+
+	public void EnterIdle(){
+		EnterState( State.IDLE );
 	}
 
 

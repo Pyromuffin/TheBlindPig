ea3f846a3c0eef153a740da8c21fdecc0250e6a7
Loveschach <codymalach@hotmail.com>
2023-04-30 13:07:28 -0700

    Minor text updates

--- PatronVoice.cs
+++ PatronVoice.cs
@@ -21,7 +21,6 @@ public partial class PatronVoice : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		SetPlaying( true );
 	}
 
 	AudioStream GetRandomSound()

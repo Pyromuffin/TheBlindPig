c56d78a04657467d0738c7b740e078c6c2a451b1
skinnycoder <ronnlefeuvre@gmail.com>
2023-05-01 00:45:56 -0700

    Half time

--- ClueDirector.cs
+++ ClueDirector.cs
@@ -251,8 +251,8 @@ public class Act
 public partial class ClueDirector : Node2D
 {
 
-	[Export] float minimumOrderTime = 8;
-	[Export] float maximumOrderTime = 20;
+	[Export] float minimumOrderTime = 4;
+	[Export] float maximumOrderTime = 10;
 
 	[Signal]
 	public delegate void SendDialogEventHandler();
